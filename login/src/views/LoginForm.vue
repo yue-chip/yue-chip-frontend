2a0544820c575b7b4f23c6e87001962b3be8ef30
Mr.Liu <158442534@qq.com>
2023-02-09 17:29:53 +0800

    init

---
+++ login/src/views/LoginForm.vue
@@ -0,0 +1,137 @@
+<template>
+  <h2 class="mb-3 text-2xl font-bold text-center xl:text-3xl enter-x xl:text-left">
+    登录
+  </h2>
+  <Form
+    class="p-4 enter-x"
+    :model="formData"
+    ref="formRef"
+  >
+    <FormItem name="account" class="enter-x">
+      <Input
+        size="large"
+        v-model:value="formData.account"
+        placeholder="用户名"
+        class="fix-auto-fill"
+      />
+    </FormItem>
+    <FormItem name="password" class="enter-x">
+      <InputPassword
+        size="large"
+        visibilityToggle
+        v-model:value="formData.password"
+        placeholder="密码"
+      />
+    </FormItem>
+
+<!--    <ARow class="enter-x">-->
+<!--      <ACol :span="12">-->
+<!--        <FormItem>-->
+<!--          &lt;!&ndash; No logic, you need to deal with it yourself &ndash;&gt;-->
+<!--          <Checkbox v-model:checked="rememberMe" size="small">-->
+<!--            {{ t('sys.login.rememberMe') }}-->
+<!--          </Checkbox>-->
+<!--        </FormItem>-->
+<!--      </ACol>-->
+<!--      <ACol :span="12">-->
+<!--        <FormItem :style="{ 'text-align': 'right' }">-->
+<!--          &lt;!&ndash; No logic, you need to deal with it yourself &ndash;&gt;-->
+<!--          <Button type="link" size="small" @click="setLoginState(LoginStateEnum.RESET_PASSWORD)">-->
+<!--            {{ t('sys.login.forgetPassword') }}-->
+<!--          </Button>-->
+<!--        </FormItem>-->
+<!--      </ACol>-->
+<!--    </ARow>-->
+
+    <FormItem class="enter-x">
+      <Button type="primary" size="large" block :loading="loading">
+        登陆
+      </Button>
+      <!-- <Button size="large" class="mt-4 enter-x" block @click="handleRegister">
+        {{ t('sys.login.registerButton') }}
+      </Button> -->
+    </FormItem>
+<!--    <ARow class="enter-x">-->
+<!--      <ACol :md="8" :xs="24">-->
+<!--        <Button block @click="setLoginState(LoginStateEnum.MOBILE)">-->
+<!--          {{ t('sys.login.mobileSignInFormTitle') }}-->
+<!--        </Button>-->
+<!--      </ACol>-->
+<!--      <ACol :md="6" :xs="24">-->
+<!--        <Button block @click="setLoginState(LoginStateEnum.REGISTER)">-->
+<!--          {{ t('sys.login.registerButton') }}-->
+<!--        </Button>-->
+<!--      </ACol>-->
+<!--    </ARow>-->
+  </Form>
+</template>
+<script lang="ts" setup>
+  import { reactive, ref, unref, computed } from 'vue'
+
+  import { Checkbox, Form, Input, Row, Col, Button } from 'ant-design-vue'
+  // import LoginFormTitle from './LoginFormTitle.vue'
+  //
+  // import { useI18n } from '/@/hooks/web/useI18n'
+  // import { useMessage } from '/@/hooks/web/useMessage'
+  //
+  // import { useUserStore } from '/@/store/modules/user'
+  // import { LoginStateEnum, useLoginState, useFormRules, useFormValid } from './useLogin'
+  // import { useDesign } from '/@/hooks/web/useDesign'
+  // //import { onKeyStroke } from '@vueuse/core';
+  //
+  const ACol = Col
+  const ARow = Row
+  const FormItem = Form.Item
+  const InputPassword = Input.Password
+  // const { t } = useI18n()
+  // const { notification, createErrorModal } = useMessage()
+  // const { prefixCls } = useDesign('login')
+  const prefixCls = 'vben-login'
+  // const userStore = useUserStore()
+  //
+  // const { setLoginState, getLoginState } = useLoginState()
+  // const { getFormRules } = useFormRules()
+  //
+  const formRef = ref()
+  const loading = ref(false)
+  // const rememberMe = ref(false)
+
+  const formData = reactive({
+    account: '',
+    password: '',
+  })
+
+  // const { validForm } = useFormValid(formRef)
+  //
+  // //onKeyStroke('Enter', handleLogin);
+  //
+  // const getShow = computed(() => unref(getLoginState) === LoginStateEnum.LOGIN)
+  //
+  // async function handleLogin() {
+  //   const data = await validForm()
+  //   if (!data) return
+  //   try {
+  //     loading.value = true
+  //     const userInfo = await userStore.login({
+  //       password: data.password,
+  //       username: data.account,
+  //       mode: 'none', //不要默认的错误提示
+  //     })
+  //     if (userInfo) {
+  //       notification.success({
+  //         message: t('sys.login.loginSuccessTitle'),
+  //         description: `${t('sys.login.loginSuccessDesc')}: ${userInfo.realName}`,
+  //         duration: 3,
+  //       })
+  //     }
+  //   } catch (error) {
+  //     createErrorModal({
+  //       title: t('sys.api.errorTip'),
+  //       content: (error as unknown as Error).message || t('sys.api.networkExceptionMsg'),
+  //       getContainer: () => document.body.querySelector(`.${prefixCls}`) || document.body,
+  //     })
+  //   } finally {
+  //     loading.value = false
+  //   }
+  // }
+</script>
---
+++ login/vite.config.ts
@@ -0,0 +1,66 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {join,resolve} from 'path'
+import windiCss from "vite-plugin-windicss";
+// @ts-ignore
+import viteCompression from 'vite-plugin-compression';
+import fs from "fs";
+
+const project_pages = {};
+const entryPath = resolve(__dirname, "./src");
+const entrys = fs.readdirSync(entryPath).reduce((obj, dirname) => {
+  obj[dirname] = join(entryPath, dirname);
+  return obj;
+}, {});
+
+Object.keys(entrys).forEach(pageName => {
+  if (fs.lstatSync(resolve(__dirname, `src/${pageName}`)).isDirectory() && fs.existsSync(resolve(__dirname, `src/${pageName}/index.html`))) {
+    project_pages[pageName] = resolve(__dirname, `src/${pageName}/index.html`);
+  }
+});
+if (fs.existsSync(resolve(__dirname, `src/index.html`))) {
+  project_pages[''] = resolve(__dirname, `src/index.html`);
+}
+export default defineConfig(({ mode }) => {
+  let pages = { ...project_pages };
+  return {
+    root: 'src',
+    plugins: [
+      vue(),
+      windiCss({scan: {dirs: ['.'],fileExtensions: ['vue', 'js', 'ts','html']},}),
+      viteCompression({verbose: true,disable: false,threshold: 10240,algorithm: "gzip",ext: ".gz",}),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      },
+    },
+    build: {
+      // 浏览器兼容性 ‘esnext’ | 'modules'
+      target: 'modules',
+      // 小于此阈值的导入或引用资源将内联为 base64 编码， 以避免额外的http请求， 设置为 0, 可以完全禁用此项，
+      assetsInlineLimit: 4096,
+      // 启动 / 禁用 CSS 代码拆分
+      cssCodeSplit: true,
+      // 构建后是否生成 soutrce map 文件
+      sourcemap: false,
+      // 自定义底层的 Rollup 打包配置
+      outDir: '../dist',
+      rollupOptions: {
+        input: pages,
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        }
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+  }
+})
